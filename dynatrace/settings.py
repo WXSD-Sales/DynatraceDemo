ebc7df8efb771d45243d7f7b54cab3ff69a1422f
Taylor Hanson <tahanson@cisco.com>
2021-04-20 16:52:06 -0400

    initial push

--- dynatrace/settings.py
+++ dynatrace/settings.py
@@ -7,6 +7,4 @@ class Settings(object):
     secret_phrase = os.environ.get("MY_SECRET_PHRASE")
     port = os.environ.get("MY_BOT_PORT")
 
-    dd_api_key=os.environ.get("DD_API_KEY")
-    dd_application_key=os.environ.get("DD_APPLICATION_KEY")
-    dd_creator_id=os.environ.get("DD_CREATOR_ID")
+    dyna_token=os.environ.get("DYNA_TOKEN")
